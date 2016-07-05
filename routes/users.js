c3b654eb383d039be4c9fbab4a6020bbfd3eeea9
elliothimmelfarb <elliothimmelfarb@gmail.com>
2016-07-05 16:17:59 -0700

    delete

--- routes/users.js
+++ routes/users.js
@@ -8,21 +8,27 @@ router.route('/')
   .get((req, res) => {
     User.find({}, (err, users) => {
       res.status(err ? 400 : 200).send(err || users);
-    }) // TODO: add select for -password
-  })
+    }).select('-password') // TODO: add select for -password
+  });
 
 router.post('/register', (req, res) => {
   User.register(req.body, err => {
     res.status(err ? 400 : 200).send(err);
-  })
-})
+  });
+});
 
 router.post('/login', (req, res) => {
   User.authenticate(req.body, (err, user) => {
     if(err) return res.status(400).send(err);
     let token = user.generateToken;
     res.cookie('authtoken', token).send(user);
-  })
-})
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  User.findByIdAndRemove(req.params.id, err => {
+    res.status(err ? 400 : 200).send(err);
+  });
+});
 
 module.exports = router;
